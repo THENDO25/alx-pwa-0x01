fbbb864329fa348d8dad3084717a2043ceb209bc
THENDO25 <thendomukwevho1125@gmail.com>
2025-02-10 14:00:37 +0200

    1st commit

--- alx-movie-app/components/commons/Loading.tsx
+++ alx-movie-app/components/commons/Loading.tsx
@@ -3,10 +3,10 @@ const Loading: React.FC = () => {
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-white bg-opacity-10 backdrop-blur-md rounded-lg p-8 flex flex-col justify-center items-center">
         <h1 className="text-4xl md:text-6xl font-semibold text-white mb-4 animate-pulse">
-          Loading...
+          Loading&hellip;
         </h1>
         <p className="text-lg text-gray-300">
-          Please wait, we're getting the next set of movies ready for you.
+          Please wait, we&apos;re getting the next set of movies ready for you.
         </p>
       </div>
     </div>
--- alx-movie-app/src/interfaces/link.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-function MyComponent() {
-  return (
-    <Link href="/about" legacyBehavior>
-      <a>About</a>
-    </Link>
-  );
-}
